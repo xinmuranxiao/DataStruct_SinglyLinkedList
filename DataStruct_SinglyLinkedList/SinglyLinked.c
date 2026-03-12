3131e74597ae08e0898a5ea2910708144004c69b
xinmuranxiao <xzt98948364@outlook.com>
2026-03-12 16:20:33 +0800

    202603121620

--- DataStruct_SinglyLinkedList/SinglyLinked.c
+++ DataStruct_SinglyLinkedList/SinglyLinked.c
@@ -1,11 +1,100 @@
 #include"SinglyLinked.h"
 
 SinglyLinkedNode* createSinglyLinkNode(int num_) {
+	if (num_ <= 0) {
+		return NULL;
+	}
 	SinglyLinkedNode* first = (SinglyLinkedNode*)malloc(sizeof(SinglyLinkedNode));
+	scanf_s("%d", first->Data);
+	first->next = NULL;
 	SinglyLinkedNode* tempPtr = first;
 	for (int i = 1; i < num_; i++) {
 		tempPtr->next = (SinglyLinkedNode*)malloc(sizeof(SinglyLinkedNode));
 		tempPtr = tempPtr->next;
+		scanf_s("%d", &tempPtr->Data);
+		tempPtr->next = NULL;
 	}
 	return first;
+}
+
+void listSinglyLinked(SinglyLinkedNode* ptr) {
+	while (ptr != NULL) {
+		printf("%d ", ptr->Data);
+		ptr = ptr->next;
+	}
+}
+
+SinglyLinkedNode* selectSinglinkLinked(int num_, SinglyLinkedNode* ptr) {
+	while (ptr->Data != num_ && ptr->next != NULL) {
+		ptr = ptr->next;
+	}
+	if (ptr->Data !=num_) {
+		return NULL;
+	}
+	else {
+		return ptr;
+	}
+}
+
+int insertHeadSinglyLinked(int num_,SinglyLinkedNode* ptr) {
+	SinglyLinkedNode* tempPtr = (SinglyLinkedNode*)malloc(sizeof(SinglyLinkedNode));
+	tempPtr->next = ptr->next;
+	ptr->next = tempPtr;
+	return 1;
+}
+
+int insertSinglyLinked(int num_, SinglyLinkedNode* ptr) {
+	ptr = selectSinglinkLinked(num_, ptr);
+	if (ptr==NULL) {
+		return -1;
+	}
+	else {
+		SinglyLinkedNode* tempPtr = ptr->next;
+		ptr->next = (SinglyLinkedNode*)malloc(sizeof(SinglyLinkedNode));
+		scanf_s("%d",&ptr->next->Data);
+		ptr->next->next = tempPtr;
+		return 1;
+	}
+}
+
+int appendSinglyLinked(int num_, SinglyLinkedNode* ptr) {
+	while (ptr->next != NULL)ptr = ptr->next;
+	SinglyLinkedNode* tempPtr = (SinglyLinkedNode*)malloc(sizeof(SinglyLinkedNode));
+	tempPtr->Data = num_;
+	tempPtr->next = ptr->next;
+	ptr->next = tempPtr;
+	return 1;
+}
+
+int getLengthSinglyLinked(SinglyLinkedNode* ptr) {
+	int length = 0;
+	while (ptr != NULL) {
+		length++;
+	}
+	return length;
+}
+
+int deleteSinglyLinked(int num_, SinglyLinkedNode* ptr) {
+	SinglyLinkedNode* lastPtr = NULL;
+	while (ptr->Data != num_ && ptr->next != NULL) {
+		lastPtr = ptr;
+		ptr = ptr->next;
+	}
+	if (ptr->Data != num_||lastPtr==NULL) {
+		return -1;
+	}
+	else {
+		lastPtr->next = ptr->next;
+		free(ptr);
+		return 1;
+	}
+}
+
+int freeSinglyLinked(SinglyLinkedNode* ptr) {
+	while (ptr!= NULL) {
+		SinglyLinkedNode* tempPtr = ptr;
+		ptr = ptr->next;
+		free(tempPtr);
+	}
+	return 1;
 }
--- DataStruct_SinglyLinkedList/SinglyLinked.h
+++ DataStruct_SinglyLinkedList/SinglyLinked.h
@@ -1,7 +1,23 @@
 #pragma once
 #include<stdio.h>
 
-typedef struct {
+typedef struct SinglyLinkedNode {
 	int Data;
-	SinglyLinkedNode* next;
+	struct SinglyLinkedNode* next;
 }SinglyLinkedNode;
+
+SinglyLinkedNode* createSinglyLinkNode(int num_);//创建链表
+
+void listSinglyLinked(SinglyLinkedNode* ptr);//遍历链表
+
+SinglyLinkedNode* selectSinglinkLinked(int num_, SinglyLinkedNode* ptr);//查找节点
+
+int insertHeadSinglyLinked(int num_, SinglyLinkedNode* ptr);//头部插入节点
+
+int insertSinglyLinked(int num_, SinglyLinkedNode* ptr);//指定插入
+
+int appendSinglyLinked(int num_, SinglyLinkedNode* ptr);//尾部插入节点
+
+int deleteSinglyLinked(int num_, SinglyLinkedNode* ptr);//删除节点
+
+int freeSinglyLinked(SinglyLinkedNode* ptr);//释放链表
